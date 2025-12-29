3e6b0881dcef2a06e9da852dabd62feb5e04192a
jahirulislamnahid <jahirulislamnahid10@gmail.com>
2025-12-29 21:44:26 +0600

    initial commit

--- AddEmployee.php
+++ AddEmployee.php
@@ -1,66 +1,48 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Add New Employee</title>
-    <link rel="stylesheet" href="AddEmployeeCSS.php">
-    <script src="AddEmployeeValidationJS.php"></script>
+    <head>
+        <title> Add New Employee </title>
+        <link rel="stylesheet" href="AddEmployee.css">
+        <script src="AddEmployeeValidation.js"></script>
 </head>
 <body>
 
-<div class="content">
-    <h2>Add New Employee</h2>
+<div class="contenet">
+    <h2> Add New Employee </h2>
 
-    <form class="employee-form"
-          method="POST"
-          action="AddEmployeeProcess.php"
-          onsubmit="return validateForm();">
+    <form class="employee-form" method="POST" action="AddEmployeeProcess.php" onsubmit="return validateForm();">
 
-        Username
-        <input type="text" id="username" name="username">
+    username
+    <input type="text" id="username" name="username">
+    First name
+    <input type="text" id="firstname" name="firstname">
+    Last name
+    <input type="text" id="lastname" name="lastname">
+    Email Address 
+    <input type="text" id="email" name="email">
+    Department
+    <select id="department" name="department">
+        <option value="">Select Department</option>
+        <option>HR</option>
+        <option>IT and Technology</option>
+        <option>Finance</option>
+        <option>Software</option>
+</select>
+    Salary
+    <input type="number" id="salary" name="salary">
+    Date of Birth
+    <input type="date" id="dob" name="dob">
+    Working Experience [Years]
+    <input type="number" id="experience" name="experience">
+    PASSWORD
+    <input type="password" id="password">
+    Confirm PASSWORD
+    <input type="password" id="confirmPassword">
 
-        First Name
-        <input type="text" id="firstname" name="firstname">
-
-        Last Name
-        <input type="text" id="lastname" name="lastname">
-
-        Email Address
-        <input type="email" id="email" name="email">
-
-        Department
-        <select id="department" name="department">
-            <option value="">Select Department</option>
-            <option>HR</option>
-            <option>IT And Technology</option>
-            <option>Finance</option>
-            <option>Software</option>
-        </select>
-
-        Salary
-        <input type="number" id="salary" name="salary">
-
-        Date of Birth
-        <input type="date" id="dob" name="dob">
-
-        Working Experience
-        <input type="number" id="experience" name="experience">
-
-        Password
-        <input type="password" id="password">
-
-        Confirm Password
-        <input type="password" id="confirmPassword">
-
-        <!-- ✅ SUBMIT BUTTON -->
-        <button type="submit" class="submit-btn">Submit</button>
-
-    </form>
+    <button type="submit" class="submit-btn"> Submit </button>
+</form>
 </div>
-
 </body>
 </html>
 
 
-
-
-     
