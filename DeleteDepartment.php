2f367c1207bee788383d3afc80d706f0779bc016
jahirulislamnahid <jahirulislamnahid10@gmail.com>
2026-01-01 23:09:21 +0600

    initial commit

--- DeleteDepartment.php
+++ DeleteDepartment.php
@@ -2,10 +2,9 @@
 session_start();
 
 if (isset($_POST["index"])) {
-    $index = $_POST["index"];
+    $index = $_POST["index"]
     unset($_SESSION["departments"][$index]);
 
-    // Re-index array
     $_SESSION["departments"] = array_values($_SESSION["departments"]);
 }
 
